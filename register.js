56a1bd1fb76551949a76db82356b1f50078cf6ce
Donald Atkinson <fuzzicallogic@outlook.com>
2016-01-02 10:12:05 -0700

    Initial Element Setup

---
+++ register.js
@@ -0,0 +1,32 @@
+(function registerElement(ROOT_ELEMENT) {
+
+	Polymer({
+		is: 'forge-titlebar-insert',
+
+		created: onElementCreated,
+		attached: onElementAttached,
+		ready: onElementReady,
+		detached: onElementDetached,
+
+		properties: {
+		},
+
+	});
+
+	function onElementCreated() {
+
+	}
+
+	function onElementAttached() {
+
+	}
+
+	function onElementReady() {
+
+	}
+
+	function onElementDetached() {
+
+	}
+
+}) (document.querySelector('html'));
